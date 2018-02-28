9be4fea346eb6212079dc873fa142ea3230f486f
Matt McKnett <mmcknett@gmail.com>
2018-02-28 01:32:50 -0800

    Break out Sheet into its own testable class.

--- UnitTests/Test.cs
+++ UnitTests/Test.cs
@@ -143,4 +143,136 @@ Enter command ('h' or 'help' to list commands): ";
             }
         }
     }
+
+    [TestFixture()]
+    public class SheetTests
+    {
+        [Test()]
+        public void RowCount_EmptySheetWithAnyCapacity_ReturnsZero()
+        {
+            // Arrange
+            uint anyCapacity = 10;
+
+            // Act
+            var sheet = new Sheet(anyCapacity);
+
+            // Assert
+            Assert.AreEqual(0, sheet.RowCount(), "Row count must be zero.");
+        }
+
+        [Test()]
+        public void RowCapacity_EmptySheetWithAnyCapacity_ReturnsCapacity()
+        {
+            // Arrange
+            uint anyCapacity = 10;
+
+            // Act
+            var sheet = new Sheet(anyCapacity);
+
+            // Assert
+            Assert.AreEqual(anyCapacity, sheet.RowCapacity(), "Row capacity must be 10.");
+        }
+
+        [Test()]
+        public void Insert_EmptySheetWithSomeCapacity_IncreasesRowCount()
+        {
+            // Arrange
+            uint anyCapacity = 10;
+            uint expectedRowCount = 1;
+            var rowToInsert = new Row("Test meet name", 10.0f, 20.0f);
+            var sheet = new Sheet(anyCapacity);
+
+            // Act
+            sheet.Insert(rowToInsert);
+
+            // Assert
+            Assert.AreEqual(expectedRowCount, sheet.RowCount(), "Row count must be 1.");
+        }
+
+        [Test()]
+        public void Insert_SheetWithOneElementAndSomeCapacity_RowCountIsTwo()
+        {
+            // Arrange
+            uint anyCapacity = 10;
+            uint expectedRowCount = 2;
+            var row0 = new Row("Test meet name", 10.0f, 20.0f);
+            var row1 = new Row("Second test meet name", 15.0f, 25.0f);
+            var sheet = new Sheet(anyCapacity);
+            sheet.Insert(row0);
+
+            // Act
+            sheet.Insert(row1);
+
+            // Assert
+            Assert.AreEqual(expectedRowCount, sheet.RowCount(), "Row count must be 2.");
+        }
+
+        [Test()]
+        public void Insert_SheetWithOneElementAndCapacityOne_ThrowsOutOfMemory()
+        {
+            // Arrange
+            var row0 = new Row("Test meet name", 10.0f, 20.0f);
+            var row1 = new Row("Second test meet name", 15.0f, 25.0f);
+            var sheet = new Sheet(1);
+            sheet.Insert(row0);
+
+            // Act
+            TestDelegate action = () => { sheet.Insert(row1); };
+
+            // Assert
+            Assert.Throws<OutOfMemoryException>(action, "Inserting beyond capacity must throw an exception.");
+        }
+
+        [Test()]
+        public void GetRow_SheetWithOneRow_GetsFirstRow()
+        {
+            // Arrange
+            uint anyCapacity = 10;
+            var row = new Row("Test meet name", 10.0f, 20.0f);
+            var sheet = new Sheet(anyCapacity);
+            sheet.Insert(row);
+
+            // Act
+            var actual = sheet.GetRow(0);
+
+            // Assert
+            Assert.AreEqual(row, actual, "Row must match.");
+        }
+
+        [Test()]
+        public void GetRow_SheetWithTwoRows_GetsSecondRow()
+        {
+            // Arrange
+            uint anyCapacity = 10;
+            var row0 = new Row("Test meet name", 10.0f, 20.0f);
+            var row1 = new Row("Test meet name 2", 15.0f, 25.0f);
+            var sheet = new Sheet(anyCapacity);
+            sheet.Insert(row0);
+            sheet.Insert(row1);
+
+            // Act
+            var actual = sheet.GetRow(1);
+
+            // Assert
+            Assert.AreEqual(row1, actual, "Row must match.");
+        }
+
+        [Test()]
+        public void GetRow_InvalidIndexSheetWithTwoRows_Throws()
+        {
+            // Arrange
+            uint anyCapacity = 10;
+            var row0 = new Row("Test meet name", 10.0f, 20.0f);
+            var row1 = new Row("Test meet name 2", 15.0f, 25.0f);
+            var sheet = new Sheet(anyCapacity);
+            sheet.Insert(row0);
+            sheet.Insert(row1);
+
+            // Act
+            TestDelegate action = () => { sheet.GetRow(2); };
+
+            // Assert
+            Assert.Throws<IndexOutOfRangeException>(action, "GetRow must throw.");
+        }
+    }
 }
